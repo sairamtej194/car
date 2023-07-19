25dd60ca8de11eb704b852cd9671aee8a1421da6
sairamtej194 <116157536+sairamtej194@users.noreply.github.com>
2023-07-19 12:06:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function bicycle(){
+    document.getElementById("bicycle").src="https://tse1.mm.bing.net/th?id=OIP.rU1zfV6LLEpuCNMzYTE6xAHaEo&pid=Api&P=0&h=180";
+}
+function bike(){
+    document.getElementById("bicycle").src="https://tse1.mm.bing.net/th?id=OIP.1uSYmj1quW5fg_iR8IcILQHaEu&pid=Api&P=0&h=180";
+}
+function car(){
+    document.getElementById("bicycle").src="https://tse4.mm.bing.net/th?id=OIP.q20WuuKK3O75Za_7BVPvRAHaFj&pid=Api&P=0&h=180";
+}
